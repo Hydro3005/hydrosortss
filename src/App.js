b66ed9ff967150c015cc3bad6d2aa6cd80ec99e4
Hydro3005 <armaanaerisharma@outlook.com>
2025-09-14 16:31:25 -0400

    got a logo

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import './App.css';
 function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "40px" }}>
+      <img 
+        src={`${process.env.PUBLIC_URL}/HydroSORTS.png`} 
+        alt="HydroSorts Logo" 
+        style={{ width: '150px', marginBottom: '20px' }} 
+      />
       <h1>HydroSorts</h1>
-      <p>Algorithm Visualizer coming soon...</p>
+      <p>Algorithm Visualizer coming to ur endzz soon...</p>
     </div>
   );
 }
