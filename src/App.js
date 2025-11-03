0682ec0f15204ea2a8bd0e6e21b923d01dccab86
ditim15 <ditim456@gmail.com>
2025-11-03 18:54:53 -0500

    Added Selection Sort video and summary
    Added the visualization and summary for the selection sort, embedded code still needs to be added

--- src/App.js
+++ src/App.js
@@ -23,6 +23,20 @@ function App() {
           />
         </div>
       </section>
+      <section id="Selection Sort">
+        <h1>Selection Sort</h1>
+          <video 
+            loop
+            src={`${process.env.PUBLIC_URL}/Selection Sort.mp4`}
+            controls
+            width="640"
+            height="360"
+          />z
+          <p class='summary'>A selection sort algorithm uses comparisons to sort through an array. It will repeatedly select the largest or smallest element in the unsorted part of the array and switches it with the first unsorted item.</p>
+          <p class='summary'>It starts by looking for the smallest or largest (in this case, smallest) element and then swaps it for the first element.</p>
+          <p class='summary'>Then, it will look through the remaining elements that have not been sorted, and swap it with the second element.</p>
+          <p class='summary'>This is repeated until the array is fully sorted.</p>
+      </section>
     </div>
   );
 }
